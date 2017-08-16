3d7b7ccb72cc7f998b435800fedfc5154a13cb00
Pedro <soufreitas.pedro@gmail.com>
2017-08-15 22:14:58 -0300

    Consertando erros.

--- src/execicio2a/JanelaTexto.java
+++ src/execicio2a/JanelaTexto.java
@@ -15,7 +15,9 @@ public class JanelaTexto extends JFrame {
     private JLabel etiqueta3 = new JLabel();
     private final JTextField texto;
     private final JTextField texto2;
-
+    private double valor1;
+    private double valor2;
+    
     public JanelaTexto() throws HeadlessException {
         super("Janela Texto");
         etiqueta1 = new JLabel("Primeiro número: ");
@@ -32,21 +34,33 @@ public class JanelaTexto extends JFrame {
         setLayout(new FlowLayout(FlowLayout.CENTER));
 
         add(etiqueta2);
-
-        texto2.addActionListener(new EscreveMensagem());
+        
+        texto.addActionListener(new Numero1Enter());
+        texto2.addActionListener(new Numero2Enter());
     }
 
-    private class EscreveMensagem implements ActionListener {
+    private class Numero1Enter implements ActionListener {
 
-        public EscreveMensagem() {
+        
+
+        public Numero1Enter() {
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            Double valor1 = Double.parseDouble(texto.getText());
-            Double valor2 = Double.parseDouble(texto2.getText());
-            etiqueta2.setText("Resultado da soma: " + (valor1 + valor2));
+            valor1 = Double.parseDouble(texto.getText());
+            texto2.requestFocus();
+        }
+    }
+    private class Numero2Enter implements ActionListener {
 
+        public Numero2Enter() {
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            valor2 = Double.parseDouble(texto2.getText());
+            etiqueta2.setText("Resultado da soma: " + (valor1 + valor2));
         }
     }
 }
